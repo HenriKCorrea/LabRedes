c93effb19efc8a36cb5979dded3d22cf564872fb
labredes <labredes@labredes.portoalegre.pucrsnet.br>
2018-09-12 21:20:06 -0300

    updating arpLib

--- T1_LabRedes/arpLib.c
+++ T1_LabRedes/arpLib.c
@@ -1,12 +1,45 @@
 #include "arpLib.h"
 
+int initPackets(socket_aux *socketInfo)
+{
+    // ARP Request Packet
+    /* fill payload data (incomplete ARP request example) */
+    arpReqPacket.cooked_data.payload.arp.hw_type    = htons(1);	        //Hardware type: 1(Ethernet)
+    arpReqPacket.cooked_data.payload.arp.prot_type  = htons(ETH_P_IP);	//Protocol type: IPV4(0x0800)
+    arpReqPacket.cooked_data.payload.arp.hlen       = 6;	            //Hardware Length: MAC address length (6 bytes)
+    arpReqPacket.cooked_data.payload.arp.plen       = 4;	            //Protocol Length: Length (in octets) of IPV4 address field (4 bytes)
+    arpReqPacket.cooked_data.payload.arp.operation  = htons(1);	        //Operation: 1 for Request; 2 for reply
+    memcpy(arpReqPacket.cooked_data.payload.arp.src_hwaddr, socketInfo->this_mac, ETH_ALEN);
+    //TODO: fill arp.src_paddr
+    arpReqPacket.cooked_data.payload.arp.tgt_hwaddr = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+
+    // Ethernet Header (victim MAC will be filled in sendARPReplyPacket function)
+    arpReqPacket.cooked_data.ethernet.eth_type = htons(ETH_P_ARP);
+    memcpy(arpReqPacket.cooked_data.ethernet.src_addr, socketInfo->this_mac, ETH_ALEN);
+
+    
+    // ARP Reply Packet
+    /* fill payload data (incomplete ARP request example) */
+    arpRepPacket.cooked_data.payload.arp.hw_type    = htons(1);	        //Hardware type: 1(Ethernet)
+    arpRepPacket.cooked_data.payload.arp.prot_type  = htons(ETH_P_IP);	//Protocol type: IPV4(0x0800)
+    arpRepPacket.cooked_data.payload.arp.hlen       = 6;	            //Hardware Length: MAC address length (6 bytes)
+    arpRepPacket.cooked_data.payload.arp.plen       = 4;	            //Protocol Length: Length (in octets) of IPV4 address field (4 bytes)
+    arpRepPacket.cooked_data.payload.arp.operation  = htons(2);	        //Operation: 1 for Request; 2 for reply
+    memcpy(arpReqPacket.cooked_data.payload.arp.src_hwaddr, socketInfo->this_mac, ETH_ALEN);
+    //TODO: fill arp.src_paddr
+
+    // Ethernet Header (victim MAC will be filled in sendARPReplyPacket function)
+    arpRepPacket.cooked_data.ethernet.eth_type = htons(ETH_P_ARP);
+    memcpy(arpRepPacket.cooked_data.ethernet.src_addr, socketInfo->this_mac, ETH_ALEN);
+}
+
 void printARPPacket(union eth_buffer *arpPacket)
 {
-    printf("HW Type: %d\n", arpPacket->cooked_data.payload.arp.hw_type);
-    printf("Protocol Type: %d\n", arpPacket->cooked_data.payload.arp.prot_type);
-    printf("Header Lenght: %d\n", arpPacket->cooked_data.payload.arp.hlen);
-    printf("Data Lenght: %d\n", arpPacket->cooked_data.payload.arp.plen);
-    printf("Operation: %d\n", arpPacket->cooked_data.payload.arp.operation);
+    printf("HW Type: %d\n",         arpPacket->cooked_data.payload.arp.hw_type);
+    printf("Protocol Type: %d\n",   arpPacket->cooked_data.payload.arp.prot_type);
+    printf("Header Lenght: %d\n",   arpPacket->cooked_data.payload.arp.hlen);
+    printf("Data Lenght: %d\n",     arpPacket->cooked_data.payload.arp.plen);
+    printf("Operation: %d\n",       arpPacket->cooked_data.payload.arp.operation);
     printf("Sender HA: %d.%d.%d.%d.%d.%d\n", arpPacket->cooked_data.payload.arp.src_hwaddr[0],
                                              arpPacket->cooked_data.payload.arp.src_hwaddr[1],
                                              arpPacket->cooked_data.payload.arp.src_hwaddr[2],
@@ -29,10 +62,20 @@ void printARPPacket(union eth_buffer *arpPacket)
                                        arpPacket->cooked_data.payload.arp.tgt_paddr[3]);
 }
 
-int sendARPPacket(union eth_buffer *arpPacket, socket_aux *srcSocketInfo, socket_aux *dstSocketInfo)
+int sendARPRequestPacket(socket_aux *socketInfo, uint8_t *targetIP)
 {
-    //TODO: continuar
-    //sendto(srcSocketInfo->sockfd, arpPacket, strlen(arpPacket), 0, dstSocketInfo->sockfd, );
+    memcpy(arpReqPacket.cooked_data.payload.arp.tgt_paddr, targetIP, 4);
+    printARPPacket(arpReqPacket); // debug
+    //sendto
+}
+
+int sendARPReplyPacket(socket_aux *socketInfo, uint8_t *targetIP, uint8_t *targetMAC, uint8_t *poisonIP)
+{
+    memcpy(arpReqPacket.cooked_data.payload.arp.src_paddr, poisonIP, 4);
+    memcpy(arpReqPacket.cooked_data.payload.arp.tgt_hwaddr, targetMAC, 6);
+    memcpy(arpReqPacket.cooked_data.payload.arp.tgt_paddr, targetIP, 4);
+    printARPPacket(arpReqPacket); // debug
+    //sendto
 }
 
 int rcvARPPacket(union eth_buffer *arpPacket)
--- T1_LabRedes/arpLib.h
+++ T1_LabRedes/arpLib.h
@@ -4,20 +4,27 @@
 #include "arp.h"
 #include "socketSetup.h"
 
-// TODO: colocar estaticamente endereços MAC e IP
-// da máquina vitima e do roteador
-uint8_t src_hwaddr[6];
-uint8_t src_paddr[4];
-uint8_t tgt_hwaddr[6];
-uint8_t tgt_paddr[4];
+// for prior test
+uint8_t victim_mac[6]   = {0x00, 0x00, 0x00, 0xaa, 0x00, 0x00};
+uint8_t victim_ip[4]    = {0x10, 0x00, 0x00, 0x20};
 
+static union eth_buffer arpReqPacket;
+static union eth_buffer arpRepPacket;
+
+// Function to fill ARP packets headers
+int initPackets(socket_aux *socketInfo);
+
+//TODO: create function to get my own IP
 
 // Function that prints in the console the header fields
 // related to ARP procotol
 void printARPPacket(union eth_buffer *arpPacket);
 
-// Send ARP packet
-int sendARPPacket(union eth_buffer *arpPacket, socket_aux *srcSocketInfo, socket_aux *dstSocketInfo);
+// Send ARP Request Packet
+int sendARPRequestPacket(socket_aux *socketInfo, uint8_t *targetIP);
+
+// Send ARP Reply Packet
+int sendARPReplyPacket(socket_aux *socketInfo, uint8_t *targetIP, uint8_t *targetMAC);
 
 // Receive ARP packet
 int rcvARPPacket(union eth_buffer *arpPacket);
